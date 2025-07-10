09df4aaa5c7acc658f4de37784032713beab52d2
johnnychang25678 <zjps3407@gmail.com>
2025-07-10 00:29:00 -0700

    use eprintln for err

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,14 @@ fn main() {
     // args() returns an iterator
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing args: {err}");
+        eprintln!("Problem parsing args: {err}");
         process::exit(1);
     });
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
     match minigrep::run(config) {
         Err(e) => {
-            println!("Application error: {e}");
+            eprintln!("Application error: {e}");
             process::exit(1);
         }
         _ => (),
