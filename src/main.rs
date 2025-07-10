fb953460c92ad363c9175b114e07fc6168cd4eb5
johnnychang25678 <zjps3407@gmail.com>
2025-07-09 23:00:35 -0700

    refactor use lib.rs and add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
+use minigrep::Config;
 use std::env;
-use std::fs;
+use std::process;
 
+/*
+Calling the command line parsing logic with the argument values
+Setting up any other configuration
+Calling a run function in lib.rs
+Handling the error if run returns an error
+ */
 fn main() {
     // args() returns an iterator
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
-
-    let content = fs::read_to_string(file_path).expect("Should have to read file");
-    println!("With text:\n{}", content);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing args: {err}");
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    match minigrep::run(config) {
+        Err(e) => {
+            println!("Application error: {e}");
+            process::exit(1);
+        }
+        _ => (),
+    }
+    // // with shorthanded syntax if-let
+    // if let Err(e) = run(config) {
+    //     println!("Application error: {e}");
+    //     process::exit(1);
+    // }
 }
