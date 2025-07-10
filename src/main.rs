5734c14c5fdcf285e5e422e67f056124957957b6
johnnychang25678 <zjps3407@gmail.com>
2025-07-09 22:07:38 -0700

    read args and file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // args() returns an iterator
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    let content = fs::read_to_string(file_path).expect("Should have to read file");
+    println!("With text:\n{}", content);
 }
