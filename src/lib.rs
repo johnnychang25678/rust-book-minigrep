f0b18cd433280d6000c1e23bf936da21b276c402
johnnychang25678 <zjps3407@gmail.com>
2025-07-10 00:09:39 -0700

    add search and tests

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,37 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // ? is to handle Result, return OK() if no error, else return Err from the whole function
     let content = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{}", content);
+    for found_line in search(&config.query, &content) {
+        println!("{found_line}");
+    }
     Ok(())
+}
+
+//  indicate that the returned vector should contain string slices that
+// reference slices of the argument contents (rather than the argument query).
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = vec![];
+    // Returns an iterator over the lines of a string, as string slices.
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
